3963ebdab06a51441845a96b53e27169bb4e7e15
arrahmanriju <arrahman152004@gmail.com>
2026-01-19 03:03:05 +0600

    solve chart suggestion problem

--- ExpenseTracker/ExpenseTracker/obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
+++ ExpenseTracker/ExpenseTracker/obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d957adbe0d4f77eeac9166abb9ac453d473db47e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+431a2749c841f95993a123c49f7cf5c5f010d195")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
