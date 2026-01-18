b05acbecf5ba2a83ebda83e987e0c889ec9cc5e7
ronaldhalder <ronaldhalder89@gmail.com>
2026-01-19 00:19:46 +0600

    Update UserSettings.cs

--- ExpenseTracker/ExpenseTracker/Models/UserSettings.cs
+++ ExpenseTracker/ExpenseTracker/Models/UserSettings.cs
@@ -1,6 +1,27 @@
-﻿namespace ExpenseTracker.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTracker.Models
 {
     public class UserSettings
     {
+        [Key]
+        public int SettingsId { get; set; }
+
+        [Required]
+        public string UserName { get; set; } = "User";
+
+        public string? Email { get; set; }
+
+        public string? ProfilePicture { get; set; }
+
+        [Required]
+        public decimal MonthlyBudget { get; set; } = 0;
+
+        [Required]
+        public string Currency { get; set; } = "BDT";
+
+        public string CurrencySymbol { get; set; } = "৳";
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
     }
 }
