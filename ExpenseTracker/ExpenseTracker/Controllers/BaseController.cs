1538e500fbff847ab30d3949a44171e8780ba935
ronaldhalder <ronaldhalder89@gmail.com>
2026-01-19 00:48:02 +0600

    Update BaseController.cs

--- ExpenseTracker/ExpenseTracker/Controllers/BaseController.cs
+++ ExpenseTracker/ExpenseTracker/Controllers/BaseController.cs
@@ -1,6 +1,35 @@
-﻿namespace ExpenseTracker.Controllers
+using ExpenseTracker.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Controllers
 {
-    public class BaseController
+    public class BaseController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public BaseController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+            
+            // Get currency settings and pass to all views
+            var settings = _context.UserSettings.FirstOrDefault();
+            if (settings != null)
+            {
+                ViewBag.CurrencySymbol = settings.CurrencySymbol;
+                ViewBag.Currency = settings.Currency;
+            }
+            else
+            {
+                ViewBag.CurrencySymbol = "৳";
+                ViewBag.Currency = "BDT";
+            }
+        }
     }
 }
